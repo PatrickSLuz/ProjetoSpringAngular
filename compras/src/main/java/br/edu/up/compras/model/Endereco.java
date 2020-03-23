4e36f508a311163b8e9739b6994bd533a13720b1
PatrickSLuz <patricksantos20101@gmail.com>
2020-03-23 11:02:17 -0300

    Inicio implementacao das Models

---
+++ compras/src/main/java/br/edu/up/compras/model/Endereco.java
@@ -0,0 +1,93 @@
+package br.edu.up.compras.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="endereco")
+public class Endereco {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "cep")
+	private String cep;
+	
+	@Column(name = "logradouro")
+	private String logradouro;
+	
+	@Column(name = "complemento")
+	private String complemento;
+	
+	@Column(name = "bairro")
+	private String bairro;
+	
+	@Column(name = "localidade")
+	private String localidade;
+	
+	@Column(name = "uf")
+	private char uf;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getLocalidade() {
+		return localidade;
+	}
+
+	public void setLocalidade(String localidade) {
+		this.localidade = localidade;
+	}
+
+	public char getUf() {
+		return uf;
+	}
+
+	public void setUf(char uf) {
+		this.uf = uf;
+	}
+	
+}
---
+++ compras/src/main/java/br/edu/up/compras/model/Usuario.java
@@ -0,0 +1,49 @@
+package br.edu.up.compras.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="usuario")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "setor")
+	private String setor;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSetor() {
+		return setor;
+	}
+
+	public void setSetor(String setor) {
+		this.setor = setor;
+	}
+	
+}
