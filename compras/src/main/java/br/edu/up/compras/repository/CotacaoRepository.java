4a83906273480ddbffc93468fcc706f76c113b0f
PatrickSLuz <patricksantos20101@gmail.com>
2020-05-18 09:10:48 -0300

    rename

--- compras/src/main/java/br/edu/up/compras/repository/CotacaoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.up.compras.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.edu.up.compras.model.Cotacao;
-
-public interface CotacaoRepository extends JpaRepository<Cotacao, Integer>{
-
-}
--- compras/src/main/java/br/edu/up/compras/repository/RequisicaoItensRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.up.compras.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.edu.up.compras.model.RequisicaoItens;
-
-public interface RequisicaoItensRepository extends JpaRepository<RequisicaoItens, Integer>{
-
-}
