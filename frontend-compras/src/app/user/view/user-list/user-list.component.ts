d3386cb24ea1a2fa1c3baa73ede41e073916d404
PatrickSLuz <patricksantos20101@gmail.com>
2020-06-05 00:06:29 -0300

    add list users

--- frontend-compras/src/app/user/view/user-list/user-list.component.ts
+++ frontend-compras/src/app/user/view/user-list/user-list.component.ts
@@ -2,28 +2,34 @@ import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
-import { UserListDataSource, UserListItem } from './user-list-datasource';
+import { UserListDataSource } from './user-list-datasource';
+import { UserModel } from '../../model/user-model';
+import { UserService } from '../../service/user.service';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
-export class UserListComponent implements AfterViewInit, OnInit {
+export class UserListComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
-  @ViewChild(MatTable) table: MatTable<UserListItem>;
+  @ViewChild(MatTable) table: MatTable<UserModel>;
   dataSource: UserListDataSource;
 
-  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
+  displayedColumns = ['id', 'nome', 'setor'];
 
-  ngOnInit() {
-    this.dataSource = new UserListDataSource();
+  constructor(private userService: UserService) {
   }
 
-  ngAfterViewInit() {
-    this.dataSource.sort = this.sort;
+  ngOnInit() {
+    this.userService.getAllUsers().subscribe(users => {
+      this.dataSource = new UserListDataSource(users);
+      this.configPaginator();
+    }, error => console.log(error));
+  }
+
+  configPaginator() {
     this.dataSource.paginator = this.paginator;
     this.table.dataSource = this.dataSource;
   }
